a7649c92e9f0b635c8b49428ce316eb3c0d2d411
Iyke Israel <iykeprince48@gmail.com>
2022-08-09 00:23:42 +0100

    configure server

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+export: TYPEORM_CONNECTION=postgres
+export: TYPEORM_ENTITIES=./src/moudles/domain/*/.entity.ts 
+export: TYPEORM_HOST=database-1.cvcfilodsnmj.us-east-1.rds.amazonaws.com
+export: TYPEORM_PORT=5432
+export: TYPEORM_USERNAME=postgres
+export: TYPEORM_PASSWORD=password123
+export: TYPEORM_DATABASE=postgres
+export: TYPEORM_MIGRATIONS=/src/migrations/*.ts
+export: TYPEORM_MIGRATIONS_DIR=/src/migrations/
